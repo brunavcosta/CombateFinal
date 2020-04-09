dd5e68abd02c8b88f79b5db6a6da8fde5287942b
Bruna Costa <brunafranca704@gmail.com>
2020-04-08 21:56:55 -0300

    disque

--- Prototipo/DisqueViewController.swift
+++ Prototipo/DisqueViewController.swift
@@ -8,48 +8,28 @@
 
 import UIKit
 
-class DisqueViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class DisqueViewController: UIViewController {
 
 
+    @IBAction func botaoDisque100(_ sender: UIButton) {
+        alertaCaixa()
+    }
+    
+    func alertaCaixa(){
+        let alert = UIAlertController(title: "Deseja Proceder a Denúncia?", message: "Se uma de nossas crianças está em risco, por favor continue. ", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ligar", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Não", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.tableFooterView = UIView()
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-        let alert = UIAlertController(title: "Deseja Proceder a Denúncia?", message: "Se uma de nossas crianças está em risco, por favor continue. ", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ligar", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil)
-    }
     //@IBOutlet weak var numeros: UILabel!
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    let disque = ["Disque 100"]
 
-
-
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt
-        indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
-        
-        //cell.textLabel?.text = temas[indexPath.row]
-        cell.numero.text = disque[indexPath.row]
-    
-        return cell
-        
-    }
-    
     
     
     
