7f140ee2952555e58f0b84e0823f226162e55a68
ana clara <aclarafgranato@gmail.com>
2020-04-22 16:17:33 -0300

    pdf historia

--- Prototipo/DAO.swift
+++ Prototipo/DAO.swift
@@ -22,7 +22,7 @@ static var instance = DAO()
     
     func criaHistorias() {
         let hist1 = Historia(nome: "Consentimento", image:"capa", resumo: " a importância de manifestar aprovação em situações aonde alguém toca uma criança sem a permissão dela ou de seus pais, além de deixar claro o direito pelo seu próprio corpo.", pdf: "consfinal")
-        let hist2 = Historia(nome: "Em Breve", image:"privacidade", resumo: " conhecimento do corpo humano de maneira didática e divertida, mostrando as partes do corpo, para que servem e para quem podemos mostrar.", pdf: "lista")
+        let hist2 = Historia(nome: "Em Breve", image:"privacidade", resumo: " conhecimento do corpo humano de maneira didática e divertida, mostrando as partes do corpo, para que servem e para quem podemos mostrar.", pdf: "consfinal")
         historias.append(contentsOf: [hist1, hist2])
 
     }
